b2878c38c042894e2823cf6227298d38e5aab74e
Julian Ceipek <julian.ceipek@gmail.com>
2015-04-11 15:33:29 -0700

    Added door proxy

--- Dissonance/Assets/Editor/EditorObjExporter.cs
+++ Dissonance/Assets/Editor/EditorObjExporter.cs
@@ -33,317 +33,284 @@ public class EditorObjExporter : ScriptableObject
 	private static string targetFolder = "ExportedObj";
 
 
-    private static string MeshToString(MeshFilter mf, Dictionary<string, ObjMaterial> materialList)
-    {
-        Mesh m = mf.sharedMesh;
-        Material[] mats = mf.GetComponent<Renderer>().sharedMaterials;
+	private static string MeshToString (MeshFilter mf, Dictionary<string, ObjMaterial> materialList)
+	{
+		Mesh m = mf.sharedMesh;
+		Material[] mats = mf.GetComponent<Renderer> ().sharedMaterials;
 
-        StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder ();
 
-        sb.Append("g ").Append(mf.name).Append("\n");
-        foreach(Vector3 lv in m.vertices)
-        {
-        	Vector3 wv = lv;//mf.transform.TransformPoint(lv);
+		sb.Append ("g ").Append (mf.name).Append ("\n");
+		foreach (Vector3 lv in m.vertices) {
+			Vector3 wv = lv;//mf.transform.TransformPoint(lv);
 
-        	//This is sort of ugly - inverting x-component since we're in
-        	//a different coordinate system than "everyone" is "used to".
-            sb.Append(string.Format("v {0} {1} {2}\n",-wv.x,wv.y,wv.z));
-        }
-        sb.Append("\n");
+			//This is sort of ugly - inverting x-component since we're in
+			//a different coordinate system than "everyone" is "used to".
+			sb.Append (string.Format ("v {0} {1} {2}\n", -wv.x, wv.y, wv.z));
+		}
+		sb.Append ("\n");
 
-        foreach(Vector3 lv in m.normals)
-        {
-        	Vector3 wv = mf.transform.TransformDirection(lv);
+		foreach (Vector3 lv in m.normals) {
+			Vector3 wv = mf.transform.TransformDirection (lv);
 
-            sb.Append(string.Format("vn {0} {1} {2}\n",-wv.x,wv.y,wv.z));
-        }
-        sb.Append("\n");
+			sb.Append (string.Format ("vn {0} {1} {2}\n", -wv.x, wv.y, wv.z));
+		}
+		sb.Append ("\n");
 
-        foreach(Vector3 v in m.uv)
-        {
-            sb.Append(string.Format("vt {0} {1}\n",v.x,v.y));
-        }
+		foreach (Vector3 v in m.uv) {
+			sb.Append (string.Format ("vt {0} {1}\n", v.x, v.y));
+		}
 
-        for (int material=0; material < m.subMeshCount; material ++) {
-            sb.Append("\n");
-            sb.Append("usemtl ").Append(mats[material].name).Append("\n");
-            sb.Append("usemap ").Append(mats[material].name).Append("\n");
+		for (int material=0; material < m.subMeshCount; material ++) {
+			sb.Append ("\n");
+			sb.Append ("usemtl ").Append (mats [material].name).Append ("\n");
+			sb.Append ("usemap ").Append (mats [material].name).Append ("\n");
 
-            //See if this material is already in the materiallist.
-            try
-       		{
-          		ObjMaterial objMaterial = new ObjMaterial();
+			//See if this material is already in the materiallist.
+			try {
+				ObjMaterial objMaterial = new ObjMaterial ();
 
-          		objMaterial.name = mats[material].name;
+				objMaterial.name = mats [material].name;
 
-          		if (mats[material].mainTexture)
-          			objMaterial.textureName = EditorUtility.GetAssetPath(mats[material].mainTexture);
-          		else
-          			objMaterial.textureName = null;
+				if (mats [material].mainTexture)
+					objMaterial.textureName = AssetDatabase.GetAssetPath (mats [material].mainTexture);
+				else
+					objMaterial.textureName = null;
 
-          		materialList.Add(objMaterial.name, objMaterial);
-        	}
-        	catch (ArgumentException)
-        	{
-            	//Already in the dictionary
-        	}
+				materialList.Add (objMaterial.name, objMaterial);
+			} catch (ArgumentException) {
+				//Already in the dictionary
+			}
 
 
-            int[] triangles = m.GetTriangles(material);
-            for (int i=0;i<triangles.Length;i+=3)
-            {
-            	//Because we inverted the x-component, we also needed to alter the triangle winding.
-                sb.Append(string.Format("f {1}/{1}/{1} {0}/{0}/{0} {2}/{2}/{2}\n",
-                    triangles[i]+1 + vertexOffset, triangles[i+1]+1 + normalOffset, triangles[i+2]+1 + uvOffset));
-            }
-        }
+			int[] triangles = m.GetTriangles (material);
+			for (int i=0; i<triangles.Length; i+=3) {
+				//Because we inverted the x-component, we also needed to alter the triangle winding.
+				sb.Append (string.Format ("f {1}/{1}/{1} {0}/{0}/{0} {2}/{2}/{2}\n",
+                    triangles [i] + 1 + vertexOffset, triangles [i + 1] + 1 + normalOffset, triangles [i + 2] + 1 + uvOffset));
+			}
+		}
 
-        vertexOffset += m.vertices.Length;
-        normalOffset += m.normals.Length;
-        uvOffset += m.uv.Length;
+		vertexOffset += m.vertices.Length;
+		normalOffset += m.normals.Length;
+		uvOffset += m.uv.Length;
 
-        return sb.ToString();
-    }
+		return sb.ToString ();
+	}
 
-    private static void Clear()
-    {
-    	vertexOffset = 0;
-    	normalOffset = 0;
-    	uvOffset = 0;
-    }
+	private static void Clear ()
+	{
+		vertexOffset = 0;
+		normalOffset = 0;
+		uvOffset = 0;
+	}
 
-   	private static Dictionary<string, ObjMaterial> PrepareFileWrite()
-   	{
-   		Clear();
+	private static Dictionary<string, ObjMaterial> PrepareFileWrite ()
+	{
+		Clear ();
 
-    	return new Dictionary<string, ObjMaterial>();
-   	}
+		return new Dictionary<string, ObjMaterial> ();
+	}
 
-   	private static void MaterialsToFile(Dictionary<string, ObjMaterial> materialList, string folder, string filename)
-   	{
-   		using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".mtl"))
-        {
-        	foreach( KeyValuePair<string, ObjMaterial> kvp in materialList )
-        	{
-        		sw.Write("\n");
-        		sw.Write("newmtl {0}\n", kvp.Key);
-        		sw.Write("Ka  0.6 0.6 0.6\n");
-				sw.Write("Kd  0.6 0.6 0.6\n");
-				sw.Write("Ks  0.9 0.9 0.9\n");
-				sw.Write("d  1.0\n");
-				sw.Write("Ns  0.0\n");
-				sw.Write("illum 2\n");
+	private static void MaterialsToFile (Dictionary<string, ObjMaterial> materialList, string folder, string filename)
+	{
+		using (StreamWriter sw = new StreamWriter(folder + "/" + filename + ".mtl")) {
+			foreach (KeyValuePair<string, ObjMaterial> kvp in materialList) {
+				sw.Write ("\n");
+				sw.Write ("newmtl {0}\n", kvp.Key);
+				sw.Write ("Ka  0.6 0.6 0.6\n");
+				sw.Write ("Kd  0.6 0.6 0.6\n");
+				sw.Write ("Ks  0.9 0.9 0.9\n");
+				sw.Write ("d  1.0\n");
+				sw.Write ("Ns  0.0\n");
+				sw.Write ("illum 2\n");
 
-				if (kvp.Value.textureName != null)
-				{
+				if (kvp.Value.textureName != null) {
 					string destinationFile = kvp.Value.textureName;
 
 
-					int stripIndex = destinationFile.LastIndexOf('/');//FIXME: Should be Path.PathSeparator;
+					int stripIndex = destinationFile.LastIndexOf ('/');//FIXME: Should be Path.PathSeparator;
 
-       				if (stripIndex >= 0)
-            			destinationFile = destinationFile.Substring(stripIndex + 1).Trim();
+					if (stripIndex >= 0)
+						destinationFile = destinationFile.Substring (stripIndex + 1).Trim ();
 
 
-            		string relativeFile = destinationFile;
+					string relativeFile = destinationFile;
 
-            		destinationFile = folder + "/" + destinationFile;
+					destinationFile = folder + "/" + destinationFile;
 
-					Debug.Log("Copying texture from " + kvp.Value.textureName + " to " + destinationFile);
+					Debug.Log ("Copying texture from " + kvp.Value.textureName + " to " + destinationFile);
 
-					try
-					{
+					try {
 						//Copy the source file
-						File.Copy(kvp.Value.textureName, destinationFile);
-					}
-					catch
-					{
+						File.Copy (kvp.Value.textureName, destinationFile);
+					} catch {
 
 					}
 
 
-					sw.Write("map_Kd {0}", relativeFile);
+					sw.Write ("map_Kd {0}", relativeFile);
 				}
 
-				sw.Write("\n\n\n");
-        	}
-        }
-   	}
+				sw.Write ("\n\n\n");
+			}
+		}
+	}
 
-    private static void MeshToFile(MeshFilter mf, string folder, string filename)
-    {
-    	Dictionary<string, ObjMaterial> materialList = PrepareFileWrite();
+	private static void MeshToFile (MeshFilter mf, string folder, string filename)
+	{
+		Dictionary<string, ObjMaterial> materialList = PrepareFileWrite ();
 
-        using (StreamWriter sw = new StreamWriter(folder +"/" + filename + ".obj"))
-        {
-        	sw.Write("mtllib ./" + filename + ".mtl\n");
+		using (StreamWriter sw = new StreamWriter(folder +"/" + filename + ".obj")) {
+			sw.Write ("mtllib ./" + filename + ".mtl\n");
 
-            sw.Write(MeshToString(mf, materialList));
-        }
+			sw.Write (MeshToString (mf, materialList));
+		}
 
-        MaterialsToFile(materialList, folder, filename);
-    }
+		MaterialsToFile (materialList, folder, filename);
+	}
 
-    private static void MeshesToFile(MeshFilter[] mf, string folder, string filename)
-    {
-    	Dictionary<string, ObjMaterial> materialList = PrepareFileWrite();
+	private static void MeshesToFile (MeshFilter[] mf, string folder, string filename)
+	{
+		Dictionary<string, ObjMaterial> materialList = PrepareFileWrite ();
 
-        using (StreamWriter sw = new StreamWriter(folder +"/" + filename + ".obj"))
-        {
-        	sw.Write("mtllib ./" + filename + ".mtl\n");
+		using (StreamWriter sw = new StreamWriter(folder +"/" + filename + ".obj")) {
+			sw.Write ("mtllib ./" + filename + ".mtl\n");
 
-        	for (int i = 0; i < mf.Length; i++)
-        	{
-            	sw.Write(MeshToString(mf[i], materialList));
-            }
-        }
+			for (int i = 0; i < mf.Length; i++) {
+				sw.Write (MeshToString (mf [i], materialList));
+			}
+		}
 
-        MaterialsToFile(materialList, folder, filename);
-    }
+		MaterialsToFile (materialList, folder, filename);
+	}
 
-    private static bool CreateTargetFolder()
-    {
-    	try
-    	{
-    		System.IO.Directory.CreateDirectory(targetFolder);
-    	}
-    	catch
-    	{
-    		EditorUtility.DisplayDialog("Error!", "Failed to create target folder!", "");
-    		return false;
-    	}
+	private static bool CreateTargetFolder ()
+	{
+		try {
+			System.IO.Directory.CreateDirectory (targetFolder);
+		} catch {
+			EditorUtility.DisplayDialog ("Error!", "Failed to create target folder!", "");
+			return false;
+		}
 
-    	return true;
-    }
+		return true;
+	}
 
-    [MenuItem ("Dissonance/Export/Export models with object centers")]
-    static void ExportSelectionToSeparate()
-    {
-    	if (!CreateTargetFolder())
-    		return;
+	[MenuItem ("Dissonance/Export/Export models with object centers")]
+	static void ExportSelectionToSeparate ()
+	{
+		if (!CreateTargetFolder ())
+			return;
 
-        Transform[] selection = Selection.GetTransforms(SelectionMode.Editable | SelectionMode.ExcludePrefab);
+		Transform[] selection = Selection.GetTransforms (SelectionMode.Editable | SelectionMode.ExcludePrefab);
 
-        if (selection.Length == 0)
-        {
-        	EditorUtility.DisplayDialog("No source object selected!", "Please select one or more target objects", "");
-        	return;
-        }
+		if (selection.Length == 0) {
+			EditorUtility.DisplayDialog ("No source object selected!", "Please select one or more target objects", "");
+			return;
+		}
 
-        int exportedObjects = 0;
+		int exportedObjects = 0;
 
-       	for (int i = 0; i < selection.Length; i++)
-       	{
-       		Component[] meshfilter = selection[i].GetComponentsInChildren(typeof(MeshFilter));
+		for (int i = 0; i < selection.Length; i++) {
+			Component[] meshfilter = selection [i].GetComponentsInChildren (typeof(MeshFilter));
 
-       		for (int m = 0; m < meshfilter.Length; m++)
-       		{
-       			exportedObjects++;
-       			MeshToFile((MeshFilter)meshfilter[m], targetFolder, selection[i].name + "_" + i + "_" + m);
-       		}
-       	}
+			for (int m = 0; m < meshfilter.Length; m++) {
+				exportedObjects++;
+				MeshToFile ((MeshFilter)meshfilter [m], targetFolder, selection [i].name + "_" + i + "_" + m);
+			}
+		}
 
-       	if (exportedObjects > 0)
-       		EditorUtility.DisplayDialog("Objects exported", "Exported " + exportedObjects + " objects", "");
-       	else
-       		EditorUtility.DisplayDialog("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
-    }
+		if (exportedObjects > 0)
+			EditorUtility.DisplayDialog ("Objects exported", "Exported " + exportedObjects + " objects", "");
+		else
+			EditorUtility.DisplayDialog ("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
+	}
 
-    [MenuItem ("Dissonance/Export/Export whole selection to single OBJ")]
-    static void ExportWholeSelectionToSingle()
-    {
-    	if (!CreateTargetFolder())
-    		return;
+	[MenuItem ("Dissonance/Export/Export whole selection to single OBJ")]
+	static void ExportWholeSelectionToSingle ()
+	{
+		if (!CreateTargetFolder ())
+			return;
 
 
-        Transform[] selection = Selection.GetTransforms(SelectionMode.Editable | SelectionMode.ExcludePrefab);
+		Transform[] selection = Selection.GetTransforms (SelectionMode.Editable | SelectionMode.ExcludePrefab);
 
-        if (selection.Length == 0)
-        {
-        	EditorUtility.DisplayDialog("No source object selected!", "Please select one or more target objects", "");
-        	return;
-        }
+		if (selection.Length == 0) {
+			EditorUtility.DisplayDialog ("No source object selected!", "Please select one or more target objects", "");
+			return;
+		}
 
-        int exportedObjects = 0;
+		int exportedObjects = 0;
 
-        ArrayList mfList = new ArrayList();
+		ArrayList mfList = new ArrayList ();
 
-       	for (int i = 0; i < selection.Length; i++)
-       	{
-       		Component[] meshfilter = selection[i].GetComponentsInChildren(typeof(MeshFilter));
+		for (int i = 0; i < selection.Length; i++) {
+			Component[] meshfilter = selection [i].GetComponentsInChildren (typeof(MeshFilter));
 
-       		for (int m = 0; m < meshfilter.Length; m++)
-       		{
-       			exportedObjects++;
-       			mfList.Add(meshfilter[m]);
-       		}
-       	}
+			for (int m = 0; m < meshfilter.Length; m++) {
+				exportedObjects++;
+				mfList.Add (meshfilter [m]);
+			}
+		}
 
-       	if (exportedObjects > 0)
-       	{
-       		MeshFilter[] mf = new MeshFilter[mfList.Count];
+		if (exportedObjects > 0) {
+			MeshFilter[] mf = new MeshFilter[mfList.Count];
 
-       		for (int i = 0; i < mfList.Count; i++)
-       		{
-       			mf[i] = (MeshFilter)mfList[i];
-       		}
+			for (int i = 0; i < mfList.Count; i++) {
+				mf [i] = (MeshFilter)mfList [i];
+			}
 
-       		string filename = EditorApplication.currentScene + "_" + exportedObjects;
+			string filename = EditorApplication.currentScene + "_" + exportedObjects;
 
-       		int stripIndex = filename.LastIndexOf('/');//FIXME: Should be Path.PathSeparator
+			int stripIndex = filename.LastIndexOf ('/');//FIXME: Should be Path.PathSeparator
 
-       		if (stripIndex >= 0)
-            	filename = filename.Substring(stripIndex + 1).Trim();
+			if (stripIndex >= 0)
+				filename = filename.Substring (stripIndex + 1).Trim ();
 
-       		MeshesToFile(mf, targetFolder, filename);
+			MeshesToFile (mf, targetFolder, filename);
 
 
-       		EditorUtility.DisplayDialog("Objects exported", "Exported " + exportedObjects + " objects to " + filename, "");
-       	}
-       	else
-       		EditorUtility.DisplayDialog("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
-    }
+			EditorUtility.DisplayDialog ("Objects exported", "Exported " + exportedObjects + " objects to " + filename, "");
+		} else
+			EditorUtility.DisplayDialog ("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
+	}
 
 
 
-    [MenuItem ("Dissonance/Export/Export each selected to single OBJ")]
-    static void ExportEachSelectionToSingle()
-    {
-    	if (!CreateTargetFolder())
-    		return;
+	[MenuItem ("Dissonance/Export/Export each selected to single OBJ")]
+	static void ExportEachSelectionToSingle ()
+	{
+		if (!CreateTargetFolder ())
+			return;
 
-        Transform[] selection = Selection.GetTransforms(SelectionMode.Editable | SelectionMode.ExcludePrefab);
+		Transform[] selection = Selection.GetTransforms (SelectionMode.Editable | SelectionMode.ExcludePrefab);
 
-        if (selection.Length == 0)
-        {
-        	EditorUtility.DisplayDialog("No source object selected!", "Please select one or more target objects", "");
-        	return;
-        }
+		if (selection.Length == 0) {
+			EditorUtility.DisplayDialog ("No source object selected!", "Please select one or more target objects", "");
+			return;
+		}
 
-        int exportedObjects = 0;
+		int exportedObjects = 0;
 
 
-       	for (int i = 0; i < selection.Length; i++)
-       	{
-       		Component[] meshfilter = selection[i].GetComponentsInChildren(typeof(MeshFilter));
+		for (int i = 0; i < selection.Length; i++) {
+			Component[] meshfilter = selection [i].GetComponentsInChildren (typeof(MeshFilter));
 
-       		MeshFilter[] mf = new MeshFilter[meshfilter.Length];
+			MeshFilter[] mf = new MeshFilter[meshfilter.Length];
 
-       		for (int m = 0; m < meshfilter.Length; m++)
-       		{
-       			exportedObjects++;
-       			mf[m] = (MeshFilter)meshfilter[m];
-       		}
+			for (int m = 0; m < meshfilter.Length; m++) {
+				exportedObjects++;
+				mf [m] = (MeshFilter)meshfilter [m];
+			}
 
-       		MeshesToFile(mf, targetFolder, selection[i].name + "_" + i);
-       	}
+			MeshesToFile (mf, targetFolder, selection [i].name + "_" + i);
+		}
 
-       	if (exportedObjects > 0)
-       	{
-       		EditorUtility.DisplayDialog("Objects exported", "Exported " + exportedObjects + " objects", "");
-       	}
-       	else
-       		EditorUtility.DisplayDialog("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
-    }
+		if (exportedObjects > 0) {
+			EditorUtility.DisplayDialog ("Objects exported", "Exported " + exportedObjects + " objects", "");
+		} else
+			EditorUtility.DisplayDialog ("Objects not exported", "Make sure at least some of your selected objects have mesh filters!", "");
+	}
 
 }
--- Dissonance/Assets/Scripts/WorldEntity2D.cs
+++ Dissonance/Assets/Scripts/WorldEntity2D.cs
@@ -2,68 +2,81 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class WorldEntity2D : MonoBehaviour {
+public class WorldEntity2D : MonoBehaviour
+{
 
 	[SerializeField]
-	PlaneOrientation _planeOrientation;
+	PlaneOrientation
+		_planeOrientation;
 	public PlaneOrientation Orientation {
 		get { return _planeOrientation; }
 	}
 
 	[SerializeField]
-	List<IntVector2D> _identityLocations = new List<IntVector2D>();
+	List<IntVector2D>
+		_identityLocations = new List<IntVector2D> ();
 	[SerializeField]
-	IntVector2D _loc = new IntVector2D();
+	IntVector2D
+		_loc = new IntVector2D ();
 	public IntVector2D Location {
 		get { return _loc; }
 		set { _loc = value; }
 	}
 
-	void Start () {
-		WorldManager.g.RegisterEntity(this);
+	void Start ()
+	{
+		WorldManager.g.RegisterEntity (this);
 	}
 
-	public delegate void SimulatorDelegates();
-    public SimulatorDelegates Simulators;
+	public delegate void SimulatorDelegates ();
+	public SimulatorDelegates Simulators;
 
-	public void Simulate() {
+	public void Simulate ()
+	{
 		if (Simulators != null) {
-			Simulators();
+			Simulators ();
 		}
 	}
 
-	public List<IntVector2D> AbsoluteLocations (IntVector2D location) {
-		List<IntVector2D> absoluteLocations = new List<IntVector2D>();
+	public List<IntVector2D> AbsoluteLocations (IntVector2D location)
+	{
+		List<IntVector2D> absoluteLocations = new List<IntVector2D> ();
 		for (int i = 0; i < _identityLocations.Count; i++) {
-			absoluteLocations.Add(_identityLocations[i] + location);
+			absoluteLocations.Add (_identityLocations [i] + location);
 		}
 		return absoluteLocations;
 	}
 
-	void OnDrawGizmos () {
+	void OnDrawGizmos ()
+	{
 		// NOTE(Julian): For debug visualization in Unity editor
 		if (!Application.isPlaying && WorldManager.g != null) {
 			float tileSize = WorldManager.g.TileSize;
-			List<IntVector2D> all = AbsoluteLocations(_loc);
-			int x,y,z;
-			Gizmos.color = Color.green;
+			List<IntVector2D> all = AbsoluteLocations (_loc);
+			int x, y, z;
+			if (GetComponent<Door2D> ()) {
+				Gizmos.color = Color.cyan;
+			} else {
+				Gizmos.color = Color.green;
+			}
+
 			for (int i = 0; i < all.Count; i++) {
-				y = all[i][1];
+				y = all [i] [1];
 				switch (_planeOrientation) {
-					case PlaneOrientation.XY:
-						x = all[i][0];
-						Gizmos.DrawLine(new Vector3(tileSize * (x), tileSize * (y), 0f),
-										new Vector3(tileSize * (x + 1f), tileSize * (y + 1f), 0f));
-						Gizmos.DrawLine(new Vector3(tileSize * (x), tileSize * (y + 1f), 0f),
-										new Vector3(tileSize * (x + 1f), tileSize * (y), 0f));
-						break;
-					case PlaneOrientation.ZY:
-						z = all[i][0];
-						Gizmos.DrawLine(new Vector3(0f, tileSize * (y), tileSize * (z)),
-										new Vector3(0f, tileSize * (y + 1f), tileSize * (z + 1f)));
-						Gizmos.DrawLine(new Vector3(0f, tileSize * (y + 1f), tileSize * (z)),
-										new Vector3(0f, tileSize * (y), tileSize * (z + 1f)));
-						break;
+				case PlaneOrientation.XY:
+					x = all [i] [0];
+					Gizmos.DrawLine (new Vector3 (tileSize * (x), tileSize * (y), 0f),
+										new Vector3 (tileSize * (x + 1f), tileSize * (y + 1f), 0f));
+					Gizmos.DrawLine (new Vector3 (tileSize * (x), tileSize * (y + 1f), 0f),
+										new Vector3 (tileSize * (x + 1f), tileSize * (y), 0f));
+					break;
+				case PlaneOrientation.ZY:
+					z = all [i] [0];
+					Gizmos.DrawLine (new Vector3 (0f, tileSize * (y), tileSize * (z)),
+										new Vector3 (0f, tileSize * (y + 1f), tileSize * (z + 1f)));
+					Gizmos.DrawLine (new Vector3 (0f, tileSize * (y + 1f), tileSize * (z)),
+										new Vector3 (0f, tileSize * (y), tileSize * (z + 1f)));
+					break;
 				}
 
 			}
