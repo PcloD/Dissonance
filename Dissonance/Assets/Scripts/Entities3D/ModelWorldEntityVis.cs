eae4464d86b32c50ae166a1835a49458e6531db4
Julian Ceipek <julian.ceipek@gmail.com>
2015-04-18 18:32:27 -0700

    Implemented composability between rotation and movement

--- Dissonance/Assets/Scripts/Entities3D/ModelWorldEntityVis.cs
+++ Dissonance/Assets/Scripts/Entities3D/ModelWorldEntityVis.cs
@@ -11,13 +11,12 @@ public class ModelWorldEntityVis : MonoBehaviour {
 	// private MeshFilter _meshFilter;
 	// private MeshRenderer _meshRenderer;
 	private Transform _meshTransform;
-	private Transform _transform;
+	private Transform _rotator;
 
 	void Awake () {
 		_worldEntity = GetComponent<WorldEntity>();
 		_rotatable = GetComponent<Rotatable>();
 		_movementMachine = GetComponent<MovementMachine>();
-		_transform = transform;
 	}
 
 	void OnEnable () {
@@ -33,27 +32,35 @@ public class ModelWorldEntityVis : MonoBehaviour {
 	}
 
 	void Start () {
-		// _meshFilter = _visualChild.GetComponent<MeshFilter>();
-		// _meshRenderer = _visualChild.GetComponent<MeshRenderer>();
+		var rotatorObject = new GameObject();
+		rotatorObject.name = "Rotator";
+		_rotator = rotatorObject.transform;
+		_rotator.parent = transform;
+
 		_meshTransform = _visualChild.transform;
 		_meshTransform.parent = transform;
 		_meshTransform.position = (_worldEntity.Location.ToVector3() + Vector3.one/2f) * WorldManager.g.TileSize;
 	}
 
 	void RotationPrep () {
-		_transform.position = (_rotatable.StateInfo.rotationAnchor + Vector3.one/2f) * WorldManager.g.TileSize;
+		_meshTransform.parent = _rotator;
+		_rotator.position = (_rotatable.StateInfo.rotationAnchor + Vector3.one/2f) * WorldManager.g.TileSize;
 		_meshTransform.position = (_worldEntity.Location.ToVector3() + Vector3.one/2f) * WorldManager.g.TileSize;
 	}
 
 	void Update () {
-		if (_movementMachine != null) {
+		bool shouldRotate = ((_rotatable != null) && (_rotatable.StateInfo.state != RotationState.Idle));
+		bool shouldMove = (_movementMachine != null);
+
+		if (shouldRotate) {
+			_rotator.rotation = Quaternion.Slerp(_rotatable.StateInfo.lastRotation, _worldEntity.Rotation, _rotatable.StateInfo.fractionComplete);
+		} else if (shouldMove) {
+			_meshTransform.parent = transform;
 			_meshTransform.position = (Vector3.Lerp(_movementMachine.StateInfo.lastLocation.ToVector3(),
 													_worldEntity.Location.ToVector3(),
 													_movementMachine.StateInfo.fractionComplete) + Vector3.one/2f) * WorldManager.g.TileSize;
-		} else if (_rotatable != null) {
-			_transform.rotation = Quaternion.Slerp(_rotatable.StateInfo.lastRotation, _worldEntity.Rotation, _rotatable.StateInfo.fractionComplete);
 		} else {
-			_transform.position = (_worldEntity.Location.ToVector3() + Vector3.one/2f) * WorldManager.g.TileSize;
+			_meshTransform.parent = transform;
 			_meshTransform.position = (_worldEntity.Location.ToVector3() + Vector3.one/2f) * WorldManager.g.TileSize;
 			_meshTransform.rotation = _worldEntity.Rotation;
 		}
